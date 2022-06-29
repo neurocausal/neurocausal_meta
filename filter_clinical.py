1d34eca563748bfa81cdd89e0202e4f8a56c14c8
isil <bilginlab@gmail.com>
2022-06-29 01:05:34 +0100

    Finalize the clinical & technical term search

--- filter_clinical.py
+++ filter_clinical.py
@@ -11,12 +11,17 @@ import argparse
 import pandas as pd
 
 
+CLINICAL_VOCAB = "clinical_vocabulary.txt"
+
+TECHNICAL_VOCAB = "technical_vocabulary.txt"
+
 def count_total_word_number(row):
     full_text = row["title"] + row["abstract"] + row["body"]
     total_word_number = len(re.findall(r'\w+', full_text))
     return total_word_number
 
-def count_clinical_words(row, word):
+
+def count_term_occurrence(row, word):
     total_word_number = count_total_word_number(row)
     full_text = row["title"] + row["abstract"] + row["body"]   
     if total_word_number != 0:
@@ -25,7 +30,6 @@ def count_clinical_words(row, word):
     else:
          return 0
 
-    
 def search_clinical_vocab(row, vocab, word_thr, sum_thr):   
     n_matches = 0
     total_sum = 0
@@ -41,43 +45,84 @@ def search_clinical_vocab(row, vocab, word_thr, sum_thr):
     return False
 
 
-def main(args):
-    print('Reading file')
-    texts_df = pd.read_csv(args.text_csv).fillna("")
-    print("Read {} lines".format(len(texts_df)))
-    word_thr = args.word_threshold
-    sum_thr = args.sum_threshold
-    vocab_path= args.vocabulary
-    out_path = args.out_path if args.out_path is not None else args.text_csv.replace(
-        "text.csv", "clinical.csv")
-    print('Selection is running...')
-    with open(vocab_path, "r") as file:
-        vocab = file.readlines()
-        vocab = [word.rstrip() for word in vocab]
-    texts_df["is_paper_clinical"] = texts_df.apply(
-        search_clinical_vocab, axis=1, args=(vocab, word_thr, sum_thr))
-    print('Selection is done')
+def estimate_counts(vocab, texts_df):
     total_sum = 0
-    #print('Number of keywords {}'.format(len(vocab)))
-    temp = 0
+    #temp = 0
     for word in vocab:
-        print('Number of word iteration {}'.format(temp))
-        temp+=1
+        #print('Number of word iteration {}'.format(temp))
+        #temp+=1
         word_count = texts_df.apply(
-            count_clinical_words, axis=1, args=(word,))
+            count_term_occurrence, axis=1, args=(word,))
         texts_df[word] = word_count
         total_sum += word_count    
     texts_df["total_term_count"] = total_sum
     total_word_number = texts_df.apply(
      count_total_word_number, axis=1)
+    
     texts_df["total_word_count"] = total_word_number
+    print("total word count {}". format(texts_df.loc[0].at["total_word_count"]) ) 
+    return texts_df
+
+
+def density_of_clinical_terms(row):
+    #print("total word count {}".format(row["total_word_count"]))
+    if  row["total_word_count"] != 0:
+        if ((row["total_term_count"] * 100) / row["total_word_count"] ) > 0.1:
+            return True
+        else:
+            return False
+
+def main(args):
+    print('Reading file')
+    texts_df = pd.read_csv(args.text_csv).fillna("")
+    #print("Read {} lines".format(len(texts_df)))
+    word_thr = args.word_threshold
+    sum_thr = args.sum_threshold
+    out_clinical_path = args.out_clinical_path if args.out_clinical_path is not None else args.text_csv.replace(
+        "text.csv", "clinical.csv")
+    out_technical_path = args.out_technical_path if args.out_technical_path is not None else args.text_csv.replace(
+        "text.csv", "clinical.csv")
     
     
-    header = ["pmcid", "is_paper_clinical"] + \
-        vocab + ["total_term_count",
-                        "total_word_count"]
-    texts_df.to_csv(out_path, columns=header)
-    print(f"Results saved in {out_path}")
+    # Filter the data based to clinical set using clinical_vocabulary.
+
+    with open(CLINICAL_VOCAB, "r") as file:
+        clinical_vocab = file.readlines()
+        clinical_vocab = [word.rstrip() for word in clinical_vocab]
+        
+    print('Selection is running...') 
+    texts_df["is_paper_clinical"] = texts_df.apply(
+        search_clinical_vocab, axis=1, args=(clinical_vocab, word_thr, sum_thr))
+    print('Selection is done')
+    
+    print('Clinical word counts are estimating...')
+    texts_df = estimate_counts(clinical_vocab, texts_df)
+    print('Clinical word counts are extracted.')
+
+    
+    header = ["pmcid", "is_paper_clinical"] + clinical_vocab + ["total_term_count","total_word_count"]
+    texts_df.to_csv(out_clinical_path, columns=header)
+    print(f"Results saved in {out_clinical_path}")
+
+    
+    # Among the papers identified as clinical, search the technical term
+    # occurrence.
+    print('Technical word counts are estimating...')
+    with open(TECHNICAL_VOCAB, "r") as file:
+        tech_vocab = file.readlines()
+        tech_vocab = [word.rstrip() for word in tech_vocab] 
+    
+    texts_df = texts_df[texts_df.is_paper_clinical == True]
+    texts_df = estimate_counts(tech_vocab, texts_df)
+    texts_df["is_technical_word_appearance_above_threshold"]  = texts_df.apply(density_of_clinical_terms, axis=1)
+    print('Technical word counts are estimated.')
+
+
+    header = ["pmcid"] + \
+        tech_vocab + ["total_term_count", "total_word_count","is_technical_word_appearance_above_threshold"]
+    texts_df.to_csv(out_technical_path, columns=header)
+    print(f"Results saved in {out_technical_path}")
+    
     
     
 if __name__ == "__main__":
@@ -86,11 +131,11 @@ if __name__ == "__main__":
                         help="Path to the text.csv file containing the text of the paper from the nqdc extraction.")
     parser.add_argument("--word_threshold", "-w", type=int, required=True,
                         help="The threshold for the single keyword's appearance in a text.")
-    parser.add_argument("--vocabulary", "-v", type=str, required=True,
-                        help="The vocabulary required for the search in the text.")
     parser.add_argument("--sum_threshold", "-s", type=int, required=True,
                         help="The threshold for total number of keyword appearances in a text.")
-    parser.add_argument("--out_path", "-o", type=str,
+    parser.add_argument("--out_clinical_path", "-oc", type=str,
                         help="Output path for the csv file containing the pmcid and whether the paper is clinical.")
+    parser.add_argument("--out_technical_path", "-ot", type=str,
+                        help="Output path for the csv file containing the pmcid techical aspect of the data")
     args = parser.parse_args()
     main(args)
